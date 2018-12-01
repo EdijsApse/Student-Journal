d4f19e18f26e5e9ad491fb7a4775aa09bb42fd99
Edijs <edijsapse@gmail.com>
2018-12-01 15:04:15 +0200

    Right Sidebar layout

--- resources/views/right_sidebar.blade.php
+++ resources/views/right_sidebar.blade.php
@@ -1,1 +1,9 @@
-<p>Laba</p>
+<div class="student_list">
+    <h3>Studentu saraksts</h3>
+    <div class="student">
+        <h4>Edijs Apse</h4>
+    </div>
+    <div class="student">
+        <h4>Madara Apse</h4>
+    </div>
+</div>
