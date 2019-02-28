707c8ecbe4b7ebc324883a7fa68bd93d1257189b
Edijs <edijsapse@gmail.com>
2019-02-28 15:44:19 +0200

    Students

--- app/Students.php
+++ app/Students.php
@@ -8,4 +8,12 @@ class Students extends Model
 {
     protected $table = 'st_students';
     protected $fillable = ['name', 'surname', 'email', 'number', 'views', 'image'];
+
+    public static function getStudents(){
+        return self::orderBy("name", "asc")->paginate(12);
+    }
+
+    public function addStudent(){
+        
+    }
 }
